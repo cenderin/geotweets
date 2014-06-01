2ba0561a0213203ef10cbc756b1c199c914cad3f
cenderin <ml.alexander87@gmail.com>
2014-06-01 15:04:49 -0700

    moving around some issues

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.search(params).desc(:created_at).page(params[:page])
+
   end
 
   
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,17 +1,18 @@
 class Tweet
+
   include Mongoid::Document
   field :text, type: String
   field :location, type: Array, default: []
   field :user, type: String
   field :created_at, type: DateTime
   index({:location => "2d"}, { min: -200, max: 200 })
+  paginates_per 25
 
   def self.search(params)
-    if params[:long]== nil
-      self.all
+    if params[:long].present?
+     self.where(:location => { "$near" => [params[:long].to_f, params[:lat].to_f], '$maxDistance' => params[:radius].to_f })
     else
-    	self.where(:location => { "$near" => [params[:long].to_f, params[:lat].to_f], '$maxDistance' => params[:radius].to_f })
-
+    	self.all
     end
   end
 end
