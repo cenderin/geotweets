210470880f08f4f7bceaa858cf0c3cf71c2df1c4
cenderin <ml.alexander87@gmail.com>
2014-06-01 15:04:52 -0700

    Revert "moving around some issues"
    This reverts commit 2ba0561a0213203ef10cbc756b1c199c914cad3f.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,6 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.search(params).desc(:created_at).page(params[:page])
-
   end
 
   
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,18 +1,17 @@
 class Tweet
-
   include Mongoid::Document
   field :text, type: String
   field :location, type: Array, default: []
   field :user, type: String
   field :created_at, type: DateTime
   index({:location => "2d"}, { min: -200, max: 200 })
-  paginates_per 25
 
   def self.search(params)
-    if params[:long].present?
-     self.where(:location => { "$near" => [params[:long].to_f, params[:lat].to_f], '$maxDistance' => params[:radius].to_f })
+    if params[:long]== nil
+      self.all
     else
-    	self.all
+    	self.where(:location => { "$near" => [params[:long].to_f, params[:lat].to_f], '$maxDistance' => params[:radius].to_f })
+
     end
   end
 end
